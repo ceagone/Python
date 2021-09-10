bea90f910e56b179263d24c8f3e56a70374efd2b
Abimbola Ojetokun <bola@Abimbolas-MacBook-Pro.local>
2021-09-10 13:31:29 -0500

    second comment

--- 1st_python.py
+++ 1st_python.py
@@ -10,9 +10,6 @@ user_input = input("Hey user, enter the number of days and I will convert it to 
 user_input_number = int(user_input)
 print(days_to_units(user_input_number))
 
-<<<<<<< HEAD
 
 ## This is from branch name rebase
-=======
-## This is change from master
->>>>>>> 0e37d97462d471a1740a60a56e2b2c31c3b159bf
+## This is the second commit from rebase
